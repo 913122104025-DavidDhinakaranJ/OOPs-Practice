0218f5ba1132ad7abc649bf45424dbc8a2e24467
DAVID DHINAKARAN J <121039765+913122104025-DavidDhinakaranJ@users.noreply.github.com>
2025-12-26 16:04:50 +0530

    Changes account to be selected as choice

--- bankApp+AuthLib/bankapplication4/controllers/INetBankingController.java
+++ bankApp+AuthLib/bankapplication4/controllers/INetBankingController.java
@@ -1,7 +1,6 @@
 package com.mycompany.bankapplication4.controllers;
 
 import com.mycompany.bankapplication4.exceptions.AccountNotFoundException;
-import com.mycompany.bankapplication4.exceptions.AccountNotOwnedByUserException;
 import com.mycompany.bankapplication4.exceptions.InsufficientAmountException;
 import com.mycompany.bankapplication4.models.accounts.BankAccount;
 import java.util.List;
@@ -15,9 +14,9 @@ public interface INetBankingController {
     
     public String issueCreditCard(int pin);
     
-    public String issueDebitCard(String accNo, int pin) throws AccountNotOwnedByUserException;
+    public String issueDebitCard(String accNo, int pin);
     
-    public void transferFund(String from, String to, double transferAmount) throws AccountNotOwnedByUserException, AccountNotFoundException, InsufficientAmountException;
+    public void transferFund(String from, String to, double transferAmount) throws AccountNotFoundException, InsufficientAmountException;
     
     public double getBalance(String accNo);
 }
